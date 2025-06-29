657a4d25fd760ae0494bb5f01928e35772042e08
Petr5 <petr.mavzovin@mail.ru>
2025-06-29 13:30:12 +0300

    mock one test to fail

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(3, 4) == 7
+    assert add(3, 4) == 999
 
 def test_subtract():
     assert subtract(10, 4) == 6
